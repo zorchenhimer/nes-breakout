740045a46e6aad9a433f0090f511cd8446df9c44
Zorchenhimer <zorchenhimer@gmail.com>
2019-06-27 19:09:57 -0400

    Add some bare bones code
    This includes a makefile, linker configuration for an MMC1 SNROM, and
    some minimal code in assembly to verify it assembles.

---
+++ Makefile
@@ -0,0 +1,71 @@
+
+export PATH := $(PATH):../tools/cc65/bin
+#:../../golang/src/github.com/zorchenhimer/go-nes/cmd
+
+EXT=
+ifeq ($(OS),Windows_NT)
+EXT=.exe
+endif
+
+# Assembler and linker paths
+CA = ca65
+LD = ld65
+
+# Mapper configuration for linker
+NESCFG = nes_snrom.cfg
+
+# Tool that generates CHR data from Bitmap images
+BMP2CHR = bmp2chr$(EXT)
+
+# Tool that generates credits from an input CSV file
+GENCRED = generate-credits$(EXT)
+
+# Name of the main source file, minus the extension
+NAME = breakout
+
+# any CHR files included
+CHR = credits.chr
+
+# List of all the sources files
+SOURCES = main.asm nes2header.inc \
+		  credits.asm credits_ram.asm credits_data.i
+
+# misc
+RM = rm
+
+.PHONY: clean default cleanSym symbols pal set_pal
+
+default: all
+all: bin/$(NAME).nes
+names: clrNames credits_data.i bin/$(NAME).nes
+
+clean:
+	-$(RM) bin/*.* credits_data.i *.chr $(BMP2CHR) $(GENCRED)
+
+clrNames:
+	-$(RM) credits_data.i
+
+bin/:
+	-mkdir bin
+
+%.chr: %.bmp
+	./bmp2chr -i $< -o $@
+
+$(GENCRED): generate-credits.go
+	go build generate-credits.go
+
+bin/$(NAME).o: bin/ $(SOURCES) $(CHR)
+	$(CA) -g \
+		-t nes \
+		-o bin/$(NAME).o\
+		main.asm
+
+bin/$(NAME).nes: bin/$(NAME).o $(NESCFG)
+	$(LD) -o bin/$(NAME).nes \
+		-C $(NESCFG) \
+		--dbgfile bin/$(NAME).dbg \
+		bin/$(NAME).o
+
+credits_data.i: $(GENCRED)
+	./$(GENCRED) -x zorchenhimer -o credits_data.i -i subscriber-list.csv
+
---
+++ main.asm
@@ -0,0 +1,156 @@
+; asmsyntax=ca65
+
+.include "nes2header.inc"
+nes2mapper 1
+nes2prg 16 * 16 * 1024  ; 256k PRG
+nes2chr 0
+nes2chrram 1 * 8 * 1024 ; 8k CHR RAM
+nes2wram 1 * 8 * 1024
+nes2mirror 'V'
+nes2tv 'N'
+nes2end
+
+.segment "VECTORS"
+    .word NMI_Instr
+    .word RESET
+    .word IRQ
+
+.segment "ZEROPAGE"
+sleeping: .res1
+
+.segment "BSS"
+
+.segment "NMIRAM"
+NMI_Instr: .res 1
+NMI_Pointer: .res 2
+
+NMI_RTI = $40
+NMI_JMP = $4C
+
+.segment "OAM"
+Sprites: .res 256
+
+.segment "WRAM"
+
+.segment "PAGE00"
+.segment "PAGE01"
+.segment "PAGE02"
+.segment "PAGE03"
+.segment "PAGE04"
+.segment "PAGE05"
+.segment "PAGE06"
+.segment "PAGE07"
+.segment "PAGE08"
+.segment "PAGE09"
+.segment "PAGE10"
+.segment "PAGE11"
+.segment "PAGE12"
+.segment "PAGE13"
+.segment "PAGE14"
+
+    .incbin "credits.chr"
+
+.segment "PAGE_FIXED"
+IRQ:
+    rti
+
+RESET:
+    sei         ; Disable IRQs
+    cld         ; Disable decimal mode
+
+    ldx #$40
+    stx $4017   ; Disable APU frame IRQ
+
+    ldx #$FF
+    txs         ; Setup new stack
+
+    inx         ; Now X = 0
+
+    stx $2000   ; disable NMI
+    stx $2001   ; disable rendering
+    stx $4010   ; disable DMC IRQs
+
+    ; set NMI to just RTI
+    lda #NMI_RTI
+    sta NMI_Instr
+
+:   ; First wait for VBlank to make sure PPU is ready.
+    bit $2002   ; test this bit with ACC
+    bpl :- ; Branch on result plus
+
+:   ; Clear RAM
+    lda #$00
+    sta $0000, x
+    sta $0100, x
+    sta $0200, x
+    sta $0300, x
+    sta $0400, x
+    sta $0500, x
+    sta $0600, x
+    sta $0700, x
+
+    inx
+    bne :-  ; loop if != 0
+
+:   ; Second wait for vblank.  PPU is ready after this
+    bit $2002
+    bpl :-
+
+    jsr MMC1_Init
+
+WaitForNMI:
+    rts
+
+MMC1_Init:
+    ; Set flag
+    jsr MMC1_Select_Vert
+
+    ; select CHR 0
+    lda #%00000000
+
+    sta $A000
+    lsr a
+    sta $A000
+    lsr a
+    sta $A000
+    lsr a
+    sta $A000
+    lsr a
+    sta $A000
+    rts
+
+MMC1_Select_Vert:
+    ; control stuff
+    ; vertical mirroring, switchable $8000, fixed $C000, chr 8k
+    ; %0000 1110
+    lda #%00001110
+    jmp MMC1_Setup
+
+MMC1_Select_Horiz:
+    lda #%00001111
+
+MMC1_Setup:
+    sta $8000
+    lsr a
+    sta $8000
+    lsr a
+    sta $8000
+    lsr a
+    sta $8000
+    lsr a
+    sta $8000
+    rts
+
+MMC1_Select_Page:
+    sta $E000
+    lsr a
+    sta $E000
+    lsr a
+    sta $E000
+    lsr a
+    sta $E000
+    lsr a
+    sta $E000
+    rts
+
+
